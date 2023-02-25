33ce757ce2ef4263dc65022f50f1bc63e6abe946
gaurav singh <thakurgaurav273@gmail.com>
2023-02-25 18:20:48 +0530

    added header footer to Keeper

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+function Header(){
+    return (
+        <div className="navbar">
+            <h2>Keeper</h2>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+function Footer(){
+    return(
+        <div className="footer">
+            <h5>&copy; 2023</h5>
+        </div>
+    )
+}
+export default Footer;
